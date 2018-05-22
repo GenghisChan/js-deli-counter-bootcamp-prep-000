d5ec8fcd4d79a3880a10446e22586fc61d4273de
Christofer Chan <devchumbing@gmail.com>
2018-05-22 13:16:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+var katzDeliLine = [];
+
+function takeANumber(x, name){
+  x.push(name) //a way to get position in line index of value + 1
+  var pos = x.indexOf(name) + 1
+  return "Welcome, " + name + "." + " You are number " + pos + " in line."
+}
+
+function nowServing(x){
+  if(x === true) {
+    var person = x[0]
+    return "Currently serving " + person + "."
+    
+  }else {
+      return "There is nobody waiting to be served!"
+  }
+  x.shift(0)
+}
+
+function currentLine(line){
+  if(line === true){
+    for(var i = 0; i < line.length; i++){
+      return "The line is currently: " + (i+1) + ". " + line[0]
+    }
+  }else{
+    return "The line is currently empty."
+  }
+}
